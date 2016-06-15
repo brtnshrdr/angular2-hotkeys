20eb2017039c19f4da9a987781c3e7dc8c905be5
Stephane DION <stephane.dion@ca.thalesgroup.com>
2016-06-15 16:28:24 -0400

    fixed build because ExtendedKeyboardEvent is not exported

--- src/directives/hotkeys.directive.ts
+++ src/directives/hotkeys.directive.ts
@@ -1,18 +1,18 @@
 import {Directive, Input, OnInit, OnDestroy, ElementRef} from '@angular/core';
 import {Hotkey} from '../models/hotkey.model';
 import {HotkeysService} from '../services/hotkeys.service';
-import {ExtendedKeyboardEvent, MousetrapInstance} from 'mousetrap';
+import 'mousetrap';
 
 @Directive({
     selector : '[hotkeys]',
     providers : [HotkeysService]
 })
 export class Hotkeys implements OnInit, OnDestroy {
-    @Input('hotkeys') hotkeysInput: Array<{[combo: string]: (event: KeyboardEvent, combo: string) => ExtendedKeyboardEvent}>;
+    @Input('hotkeys') private hotkeysInput: Array<{[combo: string]: (event: KeyboardEvent, combo: string) => ExtendedKeyboardEvent}>;
 
-    mousetrap: MousetrapInstance;
-    hotkeys: Hotkey[] = [];
-    oldHotkeys: Hotkey[] = [];
+    private mousetrap: MousetrapInstance;
+    private hotkeys: Hotkey[] = [];
+    private oldHotkeys: Hotkey[] = [];
 
     constructor(private _hotkeysService: HotkeysService, private _elementRef: ElementRef) {
         this.mousetrap = new Mousetrap(this._elementRef.nativeElement); // Bind hotkeys to the current element (and any children)
