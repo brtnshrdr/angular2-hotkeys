d064859ff1e79dccdd47be817b7ff82370850790
Nick Richardson <nrichardson@istreamfs.com>
2016-09-12 16:46:27 -0500

    RC.6 compatibility, bump version numbers

--- angular2-hotkeys.ts
+++ angular2-hotkeys.ts
@@ -1,9 +1,23 @@
-import {HOTKEY_DIRECTIVES} from './src/directives';
+import {NgModule, ModuleWithProviders} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Hotkeys} from './src/directives/hotkeys.directive';
+import {HotkeysService} from './src/services/hotkeys.service';
 
-export * from './src/directives';
-export * from './src/services';
-export * from './src/models';
 
-export default {
-  directives: [HOTKEY_DIRECTIVES]
+export * from './src/directives/hotkeys.directive';
+export * from './src/services/hotkeys.service';
+export * from './src/models/hotkey.model';
+
+@NgModule({
+    imports : [CommonModule],
+    exports : [Hotkeys],
+    declarations : [Hotkeys]
+})
+export class HotkeyModule {
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule : HotkeyModule,
+            providers : [HotkeysService]
+        };
+    }
 }
