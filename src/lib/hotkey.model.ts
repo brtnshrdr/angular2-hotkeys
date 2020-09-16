c5f4d8be5528e01291b83846070950d6bfa1fa10
Sebastian Schlatow <sebastian.schlatow@cewe.de>
2020-09-16 13:45:02 +0200

    Correct hotkey jsdoc

--- src/lib/hotkey.model.ts
+++ src/lib/hotkey.model.ts
@@ -39,10 +39,10 @@ export class Hotkey {
      * Creates a new Hotkey for Mousetrap binding
      *
      * @param combo       mousetrap key binding
-     * @param description description for the help menu
      * @param callback    method to call when key is pressed
-     * @param action      the type of event to listen for (for mousetrap)
      * @param allowIn     an array of tag names to allow this combo in ('INPUT', 'SELECT', and/or 'TEXTAREA')
+     * @param description description for the help menu
+     * @param action      the type of event to listen for (for mousetrap)
      * @param persistent  if true, the binding is preserved upon route changes
      */
     constructor(public combo: string | string[], public callback: (event: KeyboardEvent, combo: string) => ExtendedKeyboardEvent | boolean,
